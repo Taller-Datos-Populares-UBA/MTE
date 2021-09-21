f03897cb3c29575e348b0341841e16d3a1fd35bf
Fanataca <fanataca@gmail.com>
2021-09-20 22:52:09 -0300

    Error 404 Agregado
    Arme una página para avisar de un error 404 en caso de que se ingrese una dirección no valida. Cree un css especial para esta pagina.

--- apps/error404.py
+++ apps/error404.py
@@ -1,3 +1,16 @@
 import dash_html_components as html
 
-layout = html.Div([html.P("ERROR 404",style={"margin":"100px"})])
+layout = html.Div([
+	html.Div(
+		id="parent-error",
+		className="parent-error",
+		children=[
+			html.Div(
+				children=[
+					html.Div(children=["Oops!"],className="error-404"),
+					html.Div(children=[html.Div("Página no encontrada.",id="page-not-found"),html.Div("Prueba a volver con la barra de navegación.",id="hint-error")], className="page-not-found")
+				],
+				className="wrapper-error")
+		],
+		)
+])
